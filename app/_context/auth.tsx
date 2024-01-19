b2b0c156018de1bfdd48dc6b9f171dde82d6ae7e
exchange-wata <exchange.wata0907@gmail.com>
2024-01-19 09:45:53 +0900

    AuthContext.Providerの実装

---
+++ app/_context/auth.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode, useContext } from 'react';
+
+interface AuthContextType {
+  login: (token: string) => void;
+  logout: () => void;
+}
+
+interface AuthProviderContext {
+  children: ReactNode;
+}
+
+const AuthContext = React.createContext<AuthContextType>({
+  login: () => {},
+  logout: () => {},
+});
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }: AuthProviderContext) => {
+  const login = (token: string) => localStorage.setItem('auth_token', token);
+  const logout = () => localStorage.removeItem('auth_token');
+  const value = { login, logout };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
