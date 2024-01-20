b4d1d3a40b4755318e996c324ed07993902c812a
exchange-wata <exchange.wata0907@gmail.com>
2024-01-20 13:24:03 +0900

    tokenの設定

--- app/_context/auth.tsx
+++ app/_context/auth.tsx
@@ -1,5 +1,6 @@
 'use client';
-import React, { ReactNode, useContext } from 'react';
+import React, { ReactNode, useContext, useEffect } from 'react';
+import apiClient from '../_lib/apiClient';
 
 interface AuthContextType {
   login: (token: string) => void;
@@ -23,6 +24,11 @@ export const AuthProvider = ({ children }: AuthProviderContext) => {
   const login = (token: string) => localStorage.setItem('auth_token', token);
   const logout = () => localStorage.removeItem('auth_token');
   const value = { login, logout };
+
+  useEffect(() => {
+    const token = localStorage.getItem('auth_token');
+    apiClient.defaults.headers['Authorization'] = `Bearer ${token}`;
+  }, []);
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
