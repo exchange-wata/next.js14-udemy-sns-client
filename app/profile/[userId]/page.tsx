3921ceba5e3fad2fd81f74493199341acdfb6b58
exchange-wata <exchange.wata0907@gmail.com>
2024-02-10 16:46:54 +0900

    エラーとローディング処理修正

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -2,61 +2,64 @@
 import { useUserProfile } from '@/app/profile/[userId]/_hooks/useUserProfile';
 import { PostType } from '@/app/types/post';
 import { notFound } from 'next/navigation';
-// import { Suspense } from 'react';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import { useUserPosts } from './_hooks/useUserPosts';
+import Error from './error';
 import Loading from './loading';
 
 const UserProfile = ({ params }: { params: { userId: string } }) => {
-  const { profile, gettingProfileError, isProfileLoading } = useUserProfile(
-    params.userId
-  );
-
-  if (gettingProfileError) notFound();
+  const { profile } = useUserProfile(params.userId);
 
   const { posts, error: gettingPostsError } = useUserPosts(params.userId);
   if (gettingPostsError) notFound();
 
-  if (isProfileLoading || !profile) return <Loading />;
-
   return (
     <div className='container mx-auto px-4 py-8'>
       <div className='w-full max-w-xl mx-auto'>
-        {/* <Suspense fallback={<Loading />}> */}
-        <div className='bg-white shadow-md rounded-lg p-6 mb-4'>
-          <div className='flex items-center'>
-            {/* TODO: next/imageに変更したい */}
-            <img
-              className='w-20 h-20 rounded-full mr-4'
-              alt='User Avatar'
-              src={profile.imageUrl}
-            />
-            <div>
-              <h2 className='text-2xl font-semibold mb-1'>
-                {profile.user.name}
-              </h2>
-              <p className='text-gray-600'>{profile.bio}</p>
-            </div>
-          </div>
-        </div>
-        {posts?.map((post: PostType) => (
-          <div className='bg-white shadow-md rounded p-4 mb-4' key={post.id}>
-            <div className='mb-4'>
-              <div className='flex items-center mb-2'>
+        <ErrorBoundary fallback={<Error />}>
+          <Suspense fallback={<Loading />}>
+            <div className='bg-white shadow-md rounded-lg p-6 mb-4'>
+              <div className='flex items-center'>
+                {/* TODO: next/imageに変更したい */}
                 <img
-                  className='w-10 h-10 rounded-full mr-2'
+                  className='w-20 h-20 rounded-full mr-4'
                   alt='User Avatar'
-                  src={profile?.imageUrl}
+                  src={profile.imageUrl}
                 />
                 <div>
-                  <h2 className='font-semibold text-md'>{post.author.name}</h2>
-                  <p className='text-gray-500 text-sm'>{post.createdAt}</p>
+                  <h2 className='text-2xl font-semibold mb-1'>
+                    {profile.user.name}
+                  </h2>
+                  <p className='text-gray-600'>{profile.bio}</p>
                 </div>
               </div>
-              <p className='text-gray-700'>{post.content}</p>
             </div>
-          </div>
-        ))}
-        {/* </Suspense> */}
+            {posts?.map((post: PostType) => (
+              <div
+                className='bg-white shadow-md rounded p-4 mb-4'
+                key={post.id}
+              >
+                <div className='mb-4'>
+                  <div className='flex items-center mb-2'>
+                    <img
+                      className='w-10 h-10 rounded-full mr-2'
+                      alt='User Avatar'
+                      src={profile?.imageUrl}
+                    />
+                    <div>
+                      <h2 className='font-semibold text-md'>
+                        {post.author.name}
+                      </h2>
+                      <p className='text-gray-500 text-sm'>{post.createdAt}</p>
+                    </div>
+                  </div>
+                  <p className='text-gray-700'>{post.content}</p>
+                </div>
+              </div>
+            ))}
+          </Suspense>
+        </ErrorBoundary>
       </div>
     </div>
   );
