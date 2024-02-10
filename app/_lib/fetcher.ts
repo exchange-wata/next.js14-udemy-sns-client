b1f6ff1676ec36dabf49705cfd663eab0d6de7fd
exchange-wata <exchange.wata0907@gmail.com>
2024-02-10 16:44:52 +0900

    型修正

--- app/_lib/fetcher.ts
+++ app/_lib/fetcher.ts
@@ -1,6 +1,6 @@
 import apiClient from './apiClient';
 
-export const fetcher = async <T>(url: string): Promise<T | null> => {
+export const fetcher = async <T>(url: string): Promise<T> => {
   const res = await apiClient.get(url);
   return res.data;
 };
--- app/profile/[userId]/_hooks/useUserProfile.tsx
+++ app/profile/[userId]/_hooks/useUserProfile.tsx
@@ -3,14 +3,14 @@ import { ProfileType } from '@/app/types/profile';
 import useSWR from 'swr';
 
 export const useUserProfile = (userId: string) => {
-  const { data, error, isLoading } = useSWR<ProfileType | null>(
+  const { data, error, isLoading } = useSWR(
     `/profile/find/${userId}`,
     fetcher<ProfileType>,
     { suspense: true }
   );
 
   return {
-    profile: !!data ? data : null,
+    profile: data,
     gettingProfileError: error,
     isProfileLoading: isLoading,
   };
